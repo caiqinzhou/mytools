dbdba4d45bf4bf9404eb7a4f305f93dbd6e528a2
caiqinzhou <caiqinzhou@gmail.com>
2012-08-21 15:19:12 +0800

    add git password cache

--- git/config-git.sh
+++ git/config-git.sh
@@ -5,5 +5,7 @@
 git config --global http.sslVerify false
 git config --global user.name "caiqinzhou"
 git config --global user.email "caiqinzhou@gmail.com"
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=7200'
 
 echo "###### Finish ######"
